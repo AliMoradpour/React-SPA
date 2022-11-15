3a9ec4e08ad14f2e57a4bfc8c07a65edf2e93628
AliMoradpour <Mr.mrdp@yahoo.com>
2022-11-14 22:26:12 -0800

    complete spa project with REACT

---
+++ precache-manifest.c36fcf24bc517d9f39bbd59f5423f9d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bae8c9dd4925045618a6",
+    "url": "/static/js/main.bae8c9dd.chunk.js"
+  },
+  {
+    "revision": "fe0897c256208d55101d",
+    "url": "/static/css/1.47c069ce.chunk.css"
+  },
+  {
+    "revision": "fe0897c256208d55101d",
+    "url": "/static/js/1.fe0897c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d71d1d373fb081f3ea851974bddc5ae2",
+    "url": "/index.html"
+  }
+];
